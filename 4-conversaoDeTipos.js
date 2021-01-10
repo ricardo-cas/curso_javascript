3cb07a7aa2e214938f98d1ff7ad52d1f103d4f1c
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-10 13:05:01 -0300

    mostrando conversao de tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversão de Tipos");
+
+console.log("Ano: " + 2020);
+console.log("2" + "2");
+
+// console.log();
+console.log("Fazendo uma conversão forçada usando o ParseInt");
+console.log(parseInt("2") + parseInt("2"));
+console.log();
