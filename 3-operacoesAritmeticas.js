7c98c4c9dea580fb46f9f6b7f099222daa09e254
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-10 12:54:31 -0300

    operações criadas

--- 3-operacoesAritmeticas.js
+++ 3-operacoesAritmeticas.js
@@ -10,3 +10,13 @@ console.log("let variável");
 console.log(
   "pois desse jeito as variaveis não ficam disponibilizadas de maneira global, isso em projetos grandes pode gerar muitos erros."
 );
+
+// Mostrando que o JavaScript pode realizar varias operações
+console.log(" Mostrando que o JavaScript pode realizar varias operações:");
+
+console.log(">>>>");
+console.log("Operações simples:");
+console.log(2 + 2);
+console.log("Operações complexas:");
+console.log(10 + 8 * 2);
+console.log("<<<<");
