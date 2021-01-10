c3c0c6611cdee041fab7f5fa9b771425a8646b92
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-10 12:50:28 -0300

    adicionado instruções e melhores praticas sobre variaveis

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,12 @@
+console.log("Operações Aritiméticas");
+
+console.log(
+  " boa pratica sempre colocar o ; no final da instrução, não é necessário mas é melhor"
+);
+
+console.log("melhor sempre declarar as variáveis desse jeito:");
+console.log("const variável");
+console.log("let variável");
+console.log(
+  "pois desse jeito as variaveis não ficam disponibilizadas de maneira global, isso em projetos grandes pode gerar muitos erros."
+);
