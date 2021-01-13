151ffad61dce9c6f34ae532c1cfbd3182d74ed69
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-12 23:03:21 -0300

    criado arquivo base para aula 6

---
+++ 6-listas.js
@@ -0,0 +1,14 @@
+console.log(`Trabalhando com listas`);
+// Declarando lista de destinos
+//  as listas são chamadas do tipo Array
+
+const listaDeDestinos = new Array(
+  `Aracaju`,
+  `Salvador`,
+  `São Paulo`,
+  `Rio de Janeiro`
+);
+
+// como imprimir a lista?
+console.log(`Destinos possíveis:`);
+console.log(listaDeDestinos);
