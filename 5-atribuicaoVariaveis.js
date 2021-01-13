bca94bfb100fd9990a57f85fbc203980a622186a
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-12 22:40:54 -0300

    Aprendi a melhor forma de utilizar a concatenacao de variaveis no meio do texto

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -2,4 +2,15 @@ console.log("Trabalhando com atribuição de variáveis: ");
 
 const idade = 28;
 const nome = "Ricardo";
+const sobrenome = "Costa";
 const ano = 2021;
+
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(
+  "Abaixo é a melhor maneira de se escrever textos utilizando variaveis"
+);
+console.log(
+  `Da forma abaixo é possível utilizar espaços no texto sem precisar concatenar com o '+'`
+);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
