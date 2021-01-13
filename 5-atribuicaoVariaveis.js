1cf0fc4a55bec0a40f152ab12a56f476234674c6
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-12 22:47:50 -0300

    adicionado informações sobre as diferençcas das variaveis do tipo let e const

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -14,3 +14,14 @@ console.log(
   `Da forma abaixo é possível utilizar espaços no texto sem precisar concatenar com o '+'`
 );
 console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log("\n");
+console.log(`**********************************`);
+console.log("\n");
+console.log(`Tipos de variáveis:`);
+console.log("\n");
+console.log(
+  `let: Este tipo permine que o seu valor seja alterado a qualquer momento`
+);
+console.log(
+  `const:  Esste tipo não permite que seja alterado seu tipo, pois é uma variável constante, ou seja, imutável`
+);
