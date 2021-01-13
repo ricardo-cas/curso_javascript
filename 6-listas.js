7546e7419fd92de46f77ea187cc45d3c619ae850
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-01-12 23:25:10 -0300

    Aprendi como remover itens de uma lista utilizando o splice

--- 6-listas.js
+++ 6-listas.js
@@ -17,3 +17,10 @@ console.log(`Para adicionar algum item dentro de uma lista {Array}\n
 listaDeDestinos.push(`Curitiba`, `Natal`, `Rio Grande o Sul`, `Minas Gerais`);
 console.log(`Destinos possíveis:`);
 console.log(listaDeDestinos);
+console.log(`\n
+Para realizar a remoção de itens de uma lista é necessário utilizar o comando: .splice([valor inicial],[quantidade de itens a serem deletados])\n
+Conforme lista abaixo:`);
+console.log(listaDeDestinos);
+console.log(`Deletando itens...`);
+listaDeDestinos.splice(0, 3);
+console.log(listaDeDestinos);
